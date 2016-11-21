a1e8b3f4b3e542d17874872c0716713c90d9d182
Canemy <sten2409@gmail.com>
2016-11-21 21:11:49 +0200

    Should work now

--- guildsite.py
+++ guildsite.py
@@ -20,11 +20,11 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 def connect_db():
     """Connects to the specific database."""
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
     rv = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
     return rv
 
