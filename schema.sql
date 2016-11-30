bd7df81d6813af99d6dc4026975bd6e9a15b1468
Canemy <sten2409@gmail.com>
2016-12-01 00:04:23 +0200

    Final SQL update

--- schema.sql
+++ schema.sql
@@ -37,10 +37,10 @@ create table progression (
 insert into progression (name, bosses, normal, heroic, mythic) values ('Emerald Nightmare', 7, 7, 7, 3);
 insert into progression (name, bosses, normal, heroic, mythic) values ('Trial of Valor', 3, 3, 0, 0);
 insert into applications (name, age, country, battletag, armory, specs, rig, experience, improve, what_it_takes, ui, logs, headset, raids, prevention, additional)
-values ('name', 'age', 'country', 'battletag', 'armory', 'specs', 'rig', 'experience', 'improve', 'what_it_takes', 'ui', 'logs', 'headset', 'raids', 'prevention', 'additional');
-insert into applications (name, age, country, battletag, armory, specs, rig, experience, improve, what_it_takes, ui, logs, headset, raids, prevention, additional)
-values ('name', 'age', 'country', 'battletag', 'armory', 'specs', 'rig', 'experience', 'improve', 'what_it_takes', 'ui', 'logs', 'headset', 'raids', 'prevention', 'additional');
-insert into applications (name, age, country, battletag, armory, specs, rig, experience, improve, what_it_takes, ui, logs, headset, raids, prevention, additional)
-values ('name', 'age', 'country', 'battletag', 'armory', 'specs', 'rig', 'experience', 'improve', 'what_it_takes', 'ui', 'logs', 'headset', 'raids', 'prevention', 'additional');
+values ('Sten', '24', 'Estonia', 'Canemy#2523', 'http://eu.battle.net/wow/en/character/twisting-nether/Canym/simple', 'Fire', 'GTX970, 500 mbit/s internet. Use it to stream and record raids',
+'Some minor raiding in TBC and WotLK. More serious in Cata. Skipped MoP. Was a raid leader and guildmaster throughout WoD. Got sick of managing retards. Joined a new guild on TN so I could be part of the problem',
+'SimCraft all my shit. Go over my logs. Class Discords. Altered Time forums',
+'My dick is just so big it needs its own 20-man raid', 'cba to link ', 'https://www.warcraftlogs.com/rankings/character/19265534/10/',
+'ofc', 'Yes', 'Getting tired of WoW', 'This is not a real application');
 insert into users (name, pw_hash)
 values ('admin', 'pbkdf2:sha1:1000$86o1V3VC$101ccd9dbea9aac4f760b9b783c4ba2f35fad8d5');
